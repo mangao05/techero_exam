173fced3d59e6a3fb79f6fbbeb6b381593b3a438
Jeric Mangao <mangao_jeric@yahoo.com>
2024-05-07 20:00:59 +0800

    remove ajax

--- index.php
+++ index.php
@@ -108,22 +108,5 @@
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
-    <script>
-        $(document).ready(function() {
-            $("#logoutBtn").click(function() {
-                $.ajax({
-                    url: 'logout.php',
-                    type: 'POST',
-                    success: function(response) {
-                       console.log(response);
-                    },
-                    error: function(xhr, status, error) {
-                        // Handle error
-                    }
-                });
-            });
-        });
-
-    </script>
 </body>
 </html>
